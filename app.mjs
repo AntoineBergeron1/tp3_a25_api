24016b296b4287eb580655eab6cb6a0ad259986f
AntoineBergeron1 <anber019@edu.cegepgarneau.ca>
2025-12-12 15:45:57 -0500

    modification de app.mjs

--- app.mjs
+++ app.mjs
@@ -40,11 +40,13 @@ app.use((error, _req, res, _next) => {
   if (error.code === 11000 && error.keyPattern) {
     const field = Object.keys(error.keyPattern)[0];
     const value = error.keyValue[field];
-    return res.status(409).json({
-      message: `Une ressource avec ce ${field} existe déjà`,
-      field,
-      value,
-    });
+    return res
+      .status(409)
+      .json({
+        message: `Une ressource avec ce ${field} existe déjà`,
+        field,
+        value,
+      });
   }
   if (error.name === "CastError" && error.path === "_id") {
     return res
@@ -77,5 +79,4 @@ mongooseConnect(
     process.exit(1);
   });
 
-// **Ne pas mettre app.listen() sur Vercel**
-module.exports = app; //Permet d'exporter l'application pour être utilisée par Vercel
+export default app;
